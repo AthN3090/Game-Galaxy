dcbe2b79d0f4be293e4ab9294eda280d71ef7b2f
AthN3090 <amandevrishi12@gmail.com>
2019-09-20 12:11:50 +0530

    ANKIT ADDED IMAGES

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ function showSlides() {
 					}
 					slides[slideIndex-1].style.display = "block";  
 					dots[slideIndex-1].className += " active";
-					setTimeout(showSlides, 7000); // Change image every 7 seconds
+					setTimeout(showSlides,2000); // Change image every 7 seconds
 				}
 
 /*this is for contactus form*/
