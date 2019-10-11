e1f55309ab40421836aee7da732e440a754450b5
AthN3090 <amandevrishi12@gmail.com>
2019-10-11 11:31:14 +0530

    RESET

--- js/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-var server = app.listen(8000,function () {
-    app.get('/',function (req,res) {
-        res.send('');
-        
-    })
-})
